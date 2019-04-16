2a1824f8c8532dfdbf489505863e35edab7a2ffd
Francisco Serrano <francisco.serrano372@gmail.com>
2019-04-15 22:42:09 -0300

    Initial commit

---
+++ preprocessing.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv('./data/raw/training.csv', encoding='ISO-8859-1')
+df.columns = ['label', 'tweet_id', 'timestamp', 'no_query', 'user', 'text']
+df = df[['label', 'timestamp', 'text']]
+
+df['label'] = df['label'].replace(0, 'Negative')
+df['label'] = df['label'].replace(4, 'Positive')
+
