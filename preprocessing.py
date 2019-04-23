2491856940b6e53c8dd7b87e103440280d4d9a08
Francisco Serrano <francisco.serrano372@gmail.com>
2019-04-23 20:27:13 -0300

    Applies basic preprocessing to text

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import re
+
 
 df = pd.read_csv('./data/raw/training.csv', encoding='ISO-8859-1')
 df.columns = ['label', 'tweet_id', 'timestamp', 'no_query', 'user', 'text']
@@ -8,4 +10,6 @@ df['label'] = df['label']\
     .replace(0, 'Negative')\
     .replace(4, 'Positive')
 
-
+df['text'] = df['text'].apply(lambda msg: re.sub('(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9-_]+)', ' ', msg))
+df['text'] = df['text'].apply(lambda msg: re.sub('[^0-9a-zA-Z]+', ' ', msg))
+df['text'] = df['text'].apply(lambda msg: msg.lower())
